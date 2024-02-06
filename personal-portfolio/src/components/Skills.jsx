ab11b7c9871e5b7c4978e0b8ac22be415cfd4a88
Valentin Dutra <valentin.dutra@fing.edu.uy>
2024-02-06 16:34:01 -0300

    done

--- personal-portfolio/src/components/Skills.jsx
+++ personal-portfolio/src/components/Skills.jsx
@@ -10,12 +10,12 @@ export const Skills = () => {
                 <Row className="justify-content-center">
                     <Col md={4}>
                         <ListGroup className="fashionable-list">
-                            <ListGroup.Item>Java</ListGroup.Item>
-                            <ListGroup.Item>JSP</ListGroup.Item>
-                            <ListGroup.Item>JPA</ListGroup.Item>
-                            <ListGroup.Item>JUnit</ListGroup.Item>
-                            <ListGroup.Item>Servlets</ListGroup.Item>
-                            <ListGroup.Item>Bootstrap</ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">Java</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">JSP</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">JPA</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">JUnit</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">Servlets</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">Bootstrap</a></ListGroup.Item>
                         </ListGroup>
                     </Col>
                     <Col md={4}>
@@ -23,18 +23,18 @@ export const Skills = () => {
                             <ListGroup.Item>Python</ListGroup.Item>
                             <ListGroup.Item>React</ListGroup.Item>
                             <ListGroup.Item>SQL</ListGroup.Item>
-                            <ListGroup.Item>Godot</ListGroup.Item>
-                            <ListGroup.Item>Regular Expressions</ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/GGJUY2024" className="link-skills" target="_blank" rel="noopener noreferrer">Godot</a></ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/Regular-Expressions" className="link-skills" target="_blank" rel="noopener noreferrer">Regular Expressions</a></ListGroup.Item>
                             <ListGroup.Item>Database Optimization</ListGroup.Item>
                         </ListGroup>
                     </Col>
                     <Col md={4}>
                         <ListGroup className="fashionable-list">
-                            <ListGroup.Item>Webservices</ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">Webservices</a></ListGroup.Item>
                             <ListGroup.Item>Design Patterns</ListGroup.Item>
                             <ListGroup.Item>GitHub</ListGroup.Item>
                             <ListGroup.Item>Scrum</ListGroup.Item>
-                            <ListGroup.Item>HTML/CSS</ListGroup.Item>
+                            <ListGroup.Item><a href="https://github.com/ValenDD/TrabajoUy" className="link-skills" target="_blank" rel="noopener noreferrer">HTML/CSS</a></ListGroup.Item>
                             <ListGroup.Item>Software Architect</ListGroup.Item>
                         </ListGroup>
                     </Col>
